fb03ff484c8988b5810bc0a237bf96937b4da8b7
iljakononenko <iljakononenko3@gmail.com>
2023-04-24 23:11:26 +0200

    decorations and services

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
+  fixHeader();
+});
+
+function fixHeader() {
+  $("header").removeClass("d-none");
+
   $("#header-fixer").css("paddingTop", $("header").outerHeight() / 2);
 
   $(window).resize(function () {
     $("#header-fixer").css("paddingTop", $("header").outerHeight() / 2);
   });
-});
+}
