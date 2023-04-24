d1b059ce4bc21d7a162ad4b84e84e23f7ccbdda6
iljakononenko <iljakononenko3@gmail.com>
2023-04-24 23:41:07 +0200

    added navigation

--- js/script.js
+++ js/script.js
@@ -1,13 +1,47 @@
 $(document).ready(function () {
   fixHeader();
+
+  $("nav .nav-link").on("click", function (e) {
+    let target = $(this).attr("data-link");
+
+    if (window.location.pathname.includes("index")) {
+      if (target == "gallery") {
+        window.location.href = window.location.pathname.replace(
+          "index",
+          "gallery"
+        );
+      }
+    } else {
+      if (target != "gallery") {
+        window.location.href = window.location.pathname.replace(
+          "gallery",
+          "index"
+        );
+      }
+    }
+
+    target = "#" + target;
+    let additional_padding = 120;
+
+    if (window.innerWidth > 768) {
+      additional_padding = 60;
+    }
+
+    $("html, body").animate(
+      {
+        scrollTop: $(target).offset().top - additional_padding,
+      },
+      0
+    );
+  });
 });
 
 function fixHeader() {
   $("header").removeClass("d-none");
 
-  $("#header-fixer").css("paddingTop", $("header").outerHeight() / 2);
+  $("#header-fixer").css("paddingTop", $("header").outerHeight());
 
   $(window).resize(function () {
-    $("#header-fixer").css("paddingTop", $("header").outerHeight() / 2);
+    $("#header-fixer").css("paddingTop", $("header").outerHeight());
   });
 }
