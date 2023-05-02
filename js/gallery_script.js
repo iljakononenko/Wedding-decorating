ff13f240305ce95c680358d405b3bfd8f148ee69
iljakononenko <iljakononenko3@gmail.com>
2023-05-02 14:38:47 +0200

    add fancybox to gallery and info to footer

--- js/gallery_script.js
+++ js/gallery_script.js
@@ -2,8 +2,14 @@ $(document).ready(function () {
   for (let i = 0; i < 30; i++) {
     $(`.gallery-col-${(i % 3) + 1}`).append(`
         <div class="gallery-item">
-            <img class="img-wrapper" src="./img/gallery/${i + 1}.png" />
+            <img class="img-wrapper" data-fancybox="gallery" src="./img/gallery/${
+              i + 1
+            }.jpg" />
         </div>
     `);
   }
+
+  Fancybox.bind("[data-fancybox]", {
+    // Your custom options
+  });
 });
